d3a655b6151c0160669dd434cd2a3b54822fc0e0
Arjit Goyal <75082382+arjit1704@users.noreply.github.com>
2021-06-23 20:16:44 +0530

    Update Arrays.cpp

--- Arrays.cpp
+++ Arrays.cpp
@@ -7,17 +7,21 @@
 using namespace std;
 
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
-    int a[MAX], N,i;
-    cin>>N;
-    for(i=1;i<=N;i++)
-    {
-        cin>>a[i];
+int main()
+{
+    int n;
+    cin >> n;
+    int array[n];
+
+    for(int i = 0; i < n; i++)
+   {
+        cin >> array[i];
     }
-    for(i=0;i<N;i++)
-    {
-        cout<<a[N-i]<<" ";
-    }   
+
+    for(int i = n - 1; i >= 0; i--)
+   {
+        cin >> array[i];
+        cout << array[i] << " ";
+    }
     return 0;
 }
